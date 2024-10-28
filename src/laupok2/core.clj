c306e624bb5df6a0da202c4383e958276b6143bd
zirtoshka <367222@edu.itmo.ru>
2024-10-28 23:56:51 +0300

    add??

--- src/laupok2/core.clj
+++ src/laupok2/core.clj
@@ -1,7 +1,54 @@
 (ns laupok2.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+;; Функции:
+
+;; добавление и удаление элементов;
+;; фильтрация;
+;; отображение (map);
+;; свертки (левая и правая);
+;; структура должна быть моноидом.
+
+
+(defprotocol Bag
+  "for multiset (bag)"
+  (add [bag element] "add elemet")
+  ;; (remove-from-bag [bag element] "delete elemet")
+  ;; (filter-bag [bag pred] "filter bag by predicat")
+  ;; (map-bag [bag f] "mapping a function to elemets")
+  ;; (fold-left-bag [bag f init] "left fold")
+  ;; (fold-right-bag [bag f init] "right fold")
+  )
+
+(defrecord TreeNode [value count left right])
+
+(def empty-bag nil)
+
+(extend-type nil
+  Bag 
+  (add [_ element] (->TreeNode element 1 nil nil)))
+
+
+;; добавление и удаление элементов;
+
+(defn add-node [node element]
+  (cond
+    (nil? node) (->TreeNode element 1 nil nil)
+    (= element (:value node)) (update node :count inc)
+    (< (hash element) (hash(:value node))) (assoc node :left (add-node (:left node) element))
+    :else (assoc node :right (add-node (:right node) element))))
+
+
+(extend-type TreeNode
+  Bag
+  (add [bag element] (add-node bag element)))
+
+(let [bagi (-> empty-bag
+              (add "1")
+              (add "2")
+              (add "1"))]
+  (println bagi))
+
+(< (hash {:kok 1}) (hash {:kok 2}))
+
--- test/laupok2/core_test.clj
+++ test/laupok2/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [laupok2.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+(deftest add-node-test
+  (testing "add elements to tree"
+    (let [tree (add-node nil 5)
+          tree2 (add-node tree 3)
+          tree3 (add-node tree2 5)]
+      (is (= (:value tree)5))
+      (is(= (:count tree3)2))
+      (is (= (:value (:left tree2))3)))))
