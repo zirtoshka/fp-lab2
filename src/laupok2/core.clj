237d378443441ebcb57542f1b1adfac0a817ec37
zirtoshka <367222@edu.itmo.ru>
2024-10-29 03:28:20 +0300

    aaaa

--- src/laupok2/core.clj
+++ src/laupok2/core.clj
@@ -1,25 +1,16 @@
 (ns laupok2.core
   (:gen-class))
 
-
-;; Функции:
-
-;; добавление и удаление элементов;
-;; фильтрация;
-;; отображение (map);
-;; свертки (левая и правая);
-;; структура должна быть моноидом.
-
-
 (defprotocol Bag
   "for multiset (bag)"
-  (add-to-bag [bag element] "add elemet")
-  (remove-from-bag [bag element] "delete elemet")
-  (filter-bag [bag pred] "filter bag by predicat")
-  (map-bag [bag f] "mapping a function to elemets")
+  (add-to-bag [bag element] "add element")
+  (remove-from-bag [bag element] "delete element")
+  (filter-bag [bag pred] "filter bag by predicate")
+  (map-bag [bag f] "mapping a function to elements")
   (fold-left-bag [bag f init] "left fold")
   (fold-right-bag [bag f init] "right fold")
-  (combine-bags [bag1 bag2]))
+  (combine-bags [bag1 bag2])
+  (compare-bags [bag1 bag2]))
 
 (defrecord TreeNode [value count left right])
 
@@ -29,26 +20,19 @@
   Bag
   (add-to-bag [_ element] (->TreeNode element 1 nil nil))
   (remove-from-bag [_ _] nil)
-  (filter [_ _] nil)
-  (map [_ _] nil)
+  (filter-bag [_ _] nil)
+  (map-bag [_ _] nil)
   (fold-left-bag [_ _ acc] acc)
   (fold-right-bag [_ _ acc] acc)
   (combine-bags [_ bag2] bag2))
-
-
-;; добавление элементов;
 
 (defn add-node
   [node element]
   (cond
     (nil? node) (->TreeNode element 1 nil nil)
     (= element (:value node)) (update node :count inc)
-    (< (hash element) (hash (:value node))) (assoc node :left (add-node (:left node) element))
+    (< element (:value node)) (assoc node :left (add-node (:left node) element))
     :else (assoc node :right (add-node (:right node) element))))
-
-(< (hash {:kok 1}) (hash {:kok 2}))
-
-;; удаление элементов;
 
 (defn merge-trees
   [left right]
@@ -64,13 +48,9 @@
     (if (> (:count node) 1)
       (update node :count dec)
       (merge-trees (:left node) (:right node)))
-    (< (hash element) (hash (:value node))) (assoc node :left (remove-node (:left node) element))
+    (< element (:value node)) (assoc node :left (remove-node (:left node) element))
     :else (assoc node :right (remove-node (:right node) element))))
 
-
-
-
-;; фильтрация;
 (defn filter-tree
   [node pred]
   (when node
@@ -80,7 +60,6 @@
         (->TreeNode (:value node) (:count node) left right)
         (merge-trees left right)))))
 
-;; отображение (map);
 (defn map-tree
   [node f]
   (when node
@@ -89,58 +68,70 @@
           right (map-tree (:right node) f)]
       (->TreeNode new-value (:count node) left right))))
 
-;; свертки (левая и правая);
 (defn fold-left-tree
   [node f acc]
   (if node
     (let [acc-left (fold-left-tree (:left node) f acc)
-          acc-node (reduce (fn [a _] (f a (:value node))) acc-left
-                           (repeat (:count node) nil))
-          acc-right (fold-left-tree (:right node) f acc-node)]
-      acc-right)
+          node-acc (reduce (fn [a _] (f a (:value node))) acc-left (range (:count node)))]
+      (fold-left-tree (:right node) f node-acc))
     acc))
 
 (defn fold-right-tree
   [node f acc]
   (if node
-    (let [acc-right (fold-right-tree (:right node) f acc)
-          acc-node (reduce (fn [a _] (f (:value node) a)) acc-right
-                           (repeat (:count node) nil))
-          acc-left (fold-right-tree (:left node) f acc-node)]
-      acc-left)
+    (let [node-acc (reduce (fn [a _] (f (:value node) a)) acc (range (:count node)))
+          acc-right (fold-right-tree (:right node) f node-acc)]
+      (fold-right-tree (:left node) f acc-right))
     acc))
 
-
-;; структура должна быть моноидом.
 (defn combine-bags
   [bag1 bag2]
   (fold-left-tree bag2 add-node bag1))
 
+(defn compare-bags
+  [bag1 bag2]
+  (cond
+    (and (nil? bag1) (nil? bag2)) true
+    (or (nil? bag1) (nil? bag2)) false
+    (and (= (:value bag1) (:value bag2))
+         (= (:count bag1) (:count bag2))
+         (compare-bags (:left bag1) (:left bag2))
+         (compare-bags (:right bag1) (:right bag2))) true
+    :else false))
 
 (extend-type TreeNode
   Bag
   (add-to-bag [bag element] (add-node bag element))
   (remove-from-bag [bag element] (remove-node bag element))
-  (filter-bag [bag element] (filter-tree bag element))
-  (map-bag [bag element] (map-tree bag element))
+  (filter-bag [bag pred] (filter-tree bag pred))
+  (map-bag [bag f] (map-tree bag f))
   (combine-bags [bag1 bag2] (combine-bags bag1 bag2)))
 
 
+(defn find-count [node value]
+  (cond
+    (nil? node) nil  ; Если узел пустой, возвращаем nil
+    (= (:value node) value) (:count node)  ; Если значение узла совпадает с искомым, возвращаем счетчик
+    (< (hash value) (hash (:value node))) (find-count (:left node) value)  ; Ищем в левом поддереве
+    :else (find-count (:right node) value)))  ; Ищем в правом поддереве
+
+;; Testing the implementation
 (let [bagi (-> empty-bag
                (add-node 5)
                (add-node 3)
                (add-node 7)
                (add-node 5))]
-  (println (filter-tree bagi #(> % 4)))
-  (println (map-tree bagi #(* % 2)))
-  (println (fold-left-tree bagi + 0))        
-  (println (fold-right-tree bagi + 0))
-  (println (combine-bags bagi (add-node empty-bag 10)))
-  )
+  ;; (println (filter-tree bagi #(> % 4)))
+  ;; (println(    :count (add-node bagi 5)               ; Проверяем корректное добавление
+  ;;          ))
+  (println( find-count (remove-node bagi 3) 3 ))
+  ;; (println (map-tree bagi #(* % 2)))
+  ;; (println (fold-left-tree bagi + 0))
+  ;; (println (fold-right-tree bagi + 0))
+  (println (combine-bags bagi (add-node empty-bag 10))))
 
-
-(let [bag1 (-> empty-bag (add-to-bag 1) (add-to-bag 2))
-      bag2 (-> empty-bag (add-to-bag 3) (add-to-bag 4))
-      bag3 (-> empty-bag (add-to-bag 5) (add-to-bag 6))]
-  (println (combine-bags bag1 (combine-bags bag2 bag3)))
-  (println (combine-bags (combine-bags bag1 bag2) bag3)))
+;; (let [bag1 (-> empty-bag (add-to-bag 1) (add-to-bag 2))
+;;       bag2 (-> empty-bag (add-to-bag 3) (add-to-bag 4))
+;;       bag3 (-> empty-bag (add-to-bag 5) (add-to-bag 6))]
+;;   (println (combine-bags bag1 (combine-bags bag2 bag3)))
+;;   (println (combine-bags (combine-bags bag1 bag2) bag3)))
--- test/laupok2/core_test.clj
+++ test/laupok2/core_test.clj
@@ -1,14 +1,71 @@
 (ns laupok2.core-test
   (:require [clojure.test :refer :all]
+            ;; [clojure.test.check :refer [quick-check]]
+            ;; [clojure.test.check.properties :as prop]
             [laupok2.core :refer :all]))
 
 
+(deftest add-remove-test
+  (let [bag (-> empty-bag (add-node 5) (add-node 3) (add-node 5))]
+    (is (= 3 (:count (add-node bag 5))))               ; Проверяем корректное добавление
+    (is (= 1 (:count (remove-node bag 5))))          ; Проверяем корректное удаление
+    (is (nil? (remove-node (remove-node bag 5) 5)))
+    )
+  )  ; Проверка полного удаления элемента
 
-(deftest add-node-test
-  (testing "add elements to tree"
-    (let [tree (add-node nil 5)
-          tree2 (add-node tree 3)
-          tree3 (add-node tree2 5)]
-      (is (= (:value tree)5))
-      (is(= (:count tree3)2))
-      (is (= (:value (:left tree2))3)))))
+;; (deftest filter-map-test
+;;   (let [bag (-> empty-bag (add-to-bag 5) (add-to-bag 3) (add-to-bag 7))
+;;         filtered-bag (filter-bag bag #(> % 4))
+;;         mapped-bag (map-bag bag #(* % 2))]
+;;     (is (= 2 (:value (first (vals filtered-bag)))))     ; Проверка фильтрации
+;;     (is (= 6 (:value (first (vals mapped-bag)))))))      ; Проверка map
+
+;; (deftest fold-test
+;;   (let [bag (-> empty-bag (add-to-bag 1) (add-to-bag 2) (add-to-bag 3))]
+;;     (is (= 6 (fold-left-bag bag + 0)))                 ; Сумма значений (fold-left)
+;;     (is (= 6 (fold-right-bag bag + 0)))))              ; Сумма значений (fold-right)
+
+;; (deftest combine-test
+;;   (let [bag1 (-> empty-bag (add-to-bag 1) (add-to-bag 2))
+;;         bag2 (-> empty-bag (add-to-bag 3) (add-to-bag 4))]
+;;     (is (= 4 (count (vals (combine-bags bag1 bag2))))))) ; Объединение двух bag
+
+
+
+
+
+;; (def neutral-element-prop
+;;   (prop/for-all [x (gen/vector (gen/choose 1 10))]
+;;                 (let [bag (reduce add-to-bag empty-bag x)]
+;;                   (= bag (combine-bags bag empty-bag)))))
+
+;; (deftest test-neutral-element
+;;   (quick-check 100 neutral-element-prop))
+
+
+
+;; (def associative-combine-prop
+;;   (prop/for-all [x (gen/vector (gen/choose 1 10))
+;;                  y (gen/vector (gen/choose 1 10))
+;;                  z (gen/vector (gen/choose 1 10))]
+;;                 (let [bag1 (reduce add-to-bag empty-bag x)
+;;                       bag2 (reduce add-to-bag empty-bag y)
+;;                       bag3 (reduce add-to-bag empty-bag z)]
+;;                   (= (combine-bags bag1 (combine-bags bag2 bag3))
+;;                      (combine-bags (combine-bags bag1 bag2) bag3)))))
+
+;; (deftest test-associativity
+;;   (quick-check 100 associative-combine-prop))
+
+
+;; (def filter-idempotent-prop
+;;   (prop/for-all [x (gen/vector (gen/choose 1 10))
+;;                  pred #(> % 5)]
+;;                 (let [bag (reduce add-to-bag empty-bag x)
+;;                       filtered-once (filter-bag bag pred)
+;;                       filtered-twice (filter-bag filtered-once pred)]
+;;                   (= filtered-once filtered-twice))))
+
+;; (deftest test-filter-idempotent
+;;   (quick-check 100 filter-idempotent-prop))
+
